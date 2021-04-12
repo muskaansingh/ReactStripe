ac74d866f54e658c98f63df4d4823be3b782b78a
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-12 11:56:38 +0530

    updation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,37 @@ function App() {
     product_by: "Facebook"
   });
 
+  const makePayment = (token) => {
+    const body = {
+      token,
+      product
+    };
+    const headers = {
+      "Content-Type": "application/json"
+    };
+    
+    return fetch(`http://localhost:3040/payment`, {
+      method: "POST",
+      headers,
+      body:JSON.stringify(body)
+    })
+    .then (response => {
+      console.log("RESPONSE", response);
+      const {status} = response;
+      console.log("Status: ",status);
+    })
+    .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
       <h3>Getting Started with React.js</h3>
-      <StripeCheckout stripeKey="" token="" name="Buy Course">
+      <StripeCheckout 
+          stripeKey={process.env.REACT_APP_KEY}
+          token={makePayment}
+          name="Buy Course"
+          amount={product.price * 100}
+      >
         <button className="btn-large red">Buy React in just $ {product.price}</button>
       </StripeCheckout>
     </div>
